98e2b387bcab1c61c6c9782cd6fca5515d18c3ea
Angela Plank <angelaplanknm@gmail.com>
2020-07-15 17:51:57 -0600

    basic form created

---
+++ User-Onboarding/src/Components/Form.js
@@ -0,0 +1,192 @@
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+import axios from "axios";
+
+export default function Form() {
+  // temporary state used to display response from API. this is not a commonly used convention
+  const [post, setPost] = useState([]);
+
+  // server error
+  const [serverError, setServerError] = useState("");
+
+  // managing state for our form inputs
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    terms: true
+  });
+
+  // control whether or not the form can be submitted if there are errors in form validation (in the useEffect)
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  // managing state for errors. empty unless inline validation (validateInput) updates key/value pair to have error
+  const [errors, setErrors] = useState({
+    name: "", // strings describing error that has occured, set from yup in schema when invalid
+    email: "",
+    password: "",
+    terms: ""
+  });
+
+  // schema used for all validation to determine whether the input is valid or not
+  const formSchema = yup.object().shape({
+    name: yup.string().required("Name is a required field"), // must include name or else error
+    email: yup
+      .string()
+      .email("Must be a valid email address")
+      .required("Must include email"), // must have string present, must be of the shape of an email
+    terms: yup.boolean().oneOf([true])
+  });
+
+  // whenever state updates, validate the entire form. if valid, then change button to be enabled.
+  useEffect(() => {
+    console.log(
+      "checking to see if all values in form state follows the rules set in formSchema"
+    );
+    formSchema.isValid(formState).then(isFormValid => {
+      console.log("is form valid?", isFormValid);
+      // <expression> ? <execute true logic> : <execute the false logic> ---> ternary
+
+      // if the form is valid, and we take the opposite --> we do not want disabled btn
+      // if the form is invalid (false) and we take the opposite (!) --> we will disable the btn
+      setButtonDisabled(!isFormValid); // disabled= false if form is valid
+    });
+  }, [formState]);
+
+  // onSubmit function
+  const formSubmit = e => {
+    e.preventDefault(); // <form> onSubmit has default behavior from HTML!
+
+    // send out POST request with obj as second param, for us that is formState.
+    // trigger .catch by changing URL in post to something invalid like "a"
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then(res => {
+        // update temp state with value from API to display in <pre>
+        setPost(res.data);
+        console.log("successful API POST!");
+
+        // clear state, could also use 'initialState' here
+        setFormState({
+          name: "",
+          email: "",
+          password: "",
+          terms: true
+        });
+
+        // if successful request, clear any server errors
+        setServerError(null);
+      })
+      .catch(err => {
+        // this is where we could create a server error in the form! if API request fails, say for authentication (that user doesn't exist in our DB),
+        // set serverError
+        setServerError("oops! something happened!");
+      });
+  };
+
+  // inline validation, validating one key/value pair at a time
+  const validateChange = e => {
+    // get the value out of schema at key "e.target.name" --> "name="
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.name === "terms" ? e.target.checked : e.target.value) // value in input
+      .then(inputIsValid => {
+        // if inputIsValid is true, then erase any errors in error state at that key/value in errors
+        setErrors({
+          ...errors,
+          [e.target.name]: ""
+        });
+      })
+      .catch(err => {
+        // if failing validation, set error in state
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0]
+        });
+      });
+  };
+
+  // onChange function
+  const inputChange = e => {
+    // use persist with async code
+    e.persist(); // necessary because we're passing the event asyncronously and we need it to exist even after this function completes (which will complete before validateChange finishes)
+    console.log("input changed!", e.target.value);
+    console.log("checkbox change", e.target.checked);
+    console.log("name of input that fired event", e.target.name); // [e.target.name]: e.target.value --> computed props
+
+    let checkboxVal = true;
+
+    if (e.target.name === "terms") {
+      checkboxVal = e.target.checked;
+    } else {
+      checkboxVal = formState.terms;
+    }
+
+    const newFormData = {
+      ...formState,
+      terms: checkboxVal,
+      [e.target.name]:
+        e.target.name === "terms" ? e.target.checked : e.target.value // // remember value of the checkbox is in "checked" and all else is "value"
+    };
+
+    validateChange(e); // for each change in input, do inline validation
+    setFormState(newFormData); // update state with new data
+  };
+
+  return (
+    <form onSubmit={formSubmit}>
+      {serverError ? <p className="error">{serverError}</p> : null}
+      <label htmlFor="name">
+        Name
+        <input
+          id="name"
+          type="text"
+          name="name"
+          onChange={inputChange}
+          value={formState.name}
+        />
+        {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
+      </label>
+      <label htmlFor="email">
+        Email
+        <input
+          id="email"
+          type="text"
+          name="email"
+          onChange={inputChange}
+          value={formState.email}
+        />
+        {errors.email.length > 0 ? (
+          <p className="error">{errors.email}</p>
+        ) : null}
+      </label>
+      <label htmlFor="password">
+        Password
+        <textarea
+          id="password"
+          name="password"
+          value={formState.password}
+          onChange={inputChange}
+        />
+      </label>
+      
+      <label htmlFor="terms" className="terms">
+        <input
+          id="terms"
+          type="checkbox"
+          name="terms"
+          checked={formState.terms}
+          onChange={inputChange}
+        />
+        Terms and Conditions
+        {errors.terms.length > 0 ? (
+          <p className="error">{errors.terms}</p>
+        ) : null}
+      </label>
+      <button type="submit" disabled={buttonDisabled}>
+        Submit
+      </button>
+      <pre>{JSON.stringify(post, null, 2)}</pre>
+    </form>
+  );
+}
